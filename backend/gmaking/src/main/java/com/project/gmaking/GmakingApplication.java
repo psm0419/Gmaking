8a8341cf52e03a060921a38b5da7457f2fb50301
현재 <phyunjae7333@gmail.com>
2025-10-02 18:45:14 +0900

    Add: 초기 세팅 파일 추가

--- backend/gmaking/src/main/java/com/project/gmaking/GmakingApplication.java
+++ backend/gmaking/src/main/java/com/project/gmaking/GmakingApplication.java
@@ -1,4 +1,4 @@
-package com._thproject.gmaking;
+package com.project.gmaking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
