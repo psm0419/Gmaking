3707731863abab1bd15e182edef4fbad214578c0
현재 <phyunjae7333@gmail.com>
2025-10-02 18:37:31 +0900

    add: 초기 폴더 구조 세팅 중(React, SpringBoot)

---
+++ backend/gmaking/src/test/java/com/_thproject/gmaking/GmakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com._thproject.gmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
