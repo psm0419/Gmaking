69c33882c2bd993ff30253445b11e056d2c3b860
현재 <phyunjae7333@gmail.com>
2025-10-02 18:37:31 +0900

    add: 초기 폴더 구조 세팅 중(React, SpringBoot)

---
+++ backend/gmaking/src/main/java/com/_thproject/gmaking/GmakingApplication.java
@@ -0,0 +1,13 @@
+package com._thproject.gmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmakingApplication.class, args);
+	}
+
+}
