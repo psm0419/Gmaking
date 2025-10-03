c5c8d78049109fa1b7c46c6f973a970e983d87db
현재 <phyunjae7333@gmail.com>
2025-10-03 21:31:52 +0900

    Add: 홈페이지 컴포넌트화

---
+++ frontend/src/components/common/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="mt-16 py-6 text-center bg-gray-800 text-gray-500 text-sm">
+            <p>AI 활용 체험 게임 플랫폼 "겜만중" | © 2025 All Rights Reserved. (컴포넌트 분리 완료)</p>
+        </footer>
+    );
+};
+
+export default Footer;
